37695cf89ba9bf0ba40693d933d79795864e615a
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-02-28 18:52:45 -0700

    Created functions for generating name and color, connected to Scaledrone chat room

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,3 +5,33 @@ const drone = new ScaleDrone(CHANNEL_ID, {
         color: getRandomColor()
     }
 });
+
+function getRandomName() {
+    const adjs = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
+    const nouns = const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
+    return (
+        adjs[Math.floor(Math.random() * adjs.length)] + "_" + nouns[Math.floor(Math.random() * nouns.length)]
+    );
+};
+
+function getRandomColor() {
+    return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+};
+
+drone.on('open', error => {
+    if (error) {
+        return console.error(error);
+    }
+    console.log('Successfully connected to Scaledrone');
+    
+    const room = drone.subscribe('observable-room');
+    room.on('open', error => {
+        if (error) {
+            return console.error(error);
+        }
+        console.log('Successfully joined room');
+    });
+});
+
+let members = [];
+
