ed80299f0219b2422583c4571492fa35c907ed7f
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-03-06 13:18:42 -0700

    Added socket.io functionality to server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
-
 const app = express();
 const bodyParser = require("body-parser");
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+users = [];
+connections = [];
 
 const PORT = process.env.PORT || 8080;
 
@@ -15,20 +18,49 @@ app.use(bodyParser.json());
 
 
 app.use(express.static("public"));
+require("./routes/routes.js")(app);
 
 
-app.get('/', function(req, res) {
- 
-    res.send('Welcome to Passport with Sequelize');
- 
+// // Get chat page
+// app.get('/chat', function (req, res) {
+//     res.sendFile(__dirname, '../app/data/public/chat.html');
+// });
+
+io.sockets.on('connection', function (socket) {
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
+
+
+    // Disconnect
+    socket.on('disconnect', function (data) {
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsernames;
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected %s sockets connected', connections.length);
+    });
+
+    // Send message
+    socket.on('send message', function(data){
+        io.sockets.emit('new message', {msg: data, user: socket.username});
+    })
+
+    // New User
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsernames();
+    });
+
+    function updateUsernames(){
+        io.sockets.emit('get users', users);
+    }
 });
- 
-
 
 //routes to be added here
 
 db.sequelize.sync({force: true}).then(function() {
-    app.listen(PORT, function() {
+    server.listen(PORT, function() {
         console.log("App listening on PORT" + PORT);
     });
 });
