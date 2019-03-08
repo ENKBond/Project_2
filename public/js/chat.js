bf591133d8af389d8584ca9d91cf5bcde8b21a59
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-03-07 18:50:20 -0700

    added new socket functionality

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,45 +1,37 @@
-// var script = document.createElement('script');
-// script.src = 'https://code.jquery.com/jquery-3.3.1.min.js';
-// script.type = 'text/javascript';
-// document.getElementsByTagName('head')[0].appendChild(script);
+// Establish conncetion to server
+// This socket variable is for the frontend (not the server)
+const socket = io.connect('http://localhost:8000');
 
-$(function () {
-    const socket = io.connect();
-    const $messageForm = $('#messageForm');
-    const $message = $('#message');
-    const $chat = $('#chat');
-    // const $messageArea = ('#messageArea');
-    const $userFormArea = $('#userFormArea');
-    const $userForm = $('#userForm');
-    const $users = $('#users');
-    const $username = $('#username');
+// Query DOM
+const message = document.getElementById('message');
+handle = document.getElementById('handle'),
+    btn = document.getElementById('send'),
+    output = document.getElementById('output'),
+    feedback = document.getElementById('feedback');
 
-    $messageForm.submit(function (e) {
-        e.preventDefault();
-        socket.emit('send message', $message.val());
-        $message.val('');
-    }); 
-
-    socket.on('new message', function (data) {
-        $chat.append('<div class="well"><strong>'+data.user+'</strong>: '+data.msg+'</div>');
+// Emit events to server
+// On send click, send chat message to server
+btn.addEventListener('click', function () {
+    socket.emit('chat', {
+        message: message.value,
+        handle: handle.value
     });
+});
 
-    $userForm.submit(function (e) {
-        e.preventDefault();
-        socket.emit('new user', $username.val(), function(data){
-            if(data){
-                $userFormArea.hide();
-                $('#messageArea').show();
-            }
-        });
-        $username.val('');
-    });
+// On key press, send handle to server
+message.addEventListener('keypress', function () {
+    socket.emit('typing', handle.value);
+});
 
-    socket.on('get users', function(data){
-        let html = '';
-        for(i = 0; i < data.length; i++){
-            html += '<li class="list-group-item">'+data[i]+'</li>'
-        }
-        $users.html(html);
-    });
+
+// Listen for events from server
+// receive chat message from server and output to DOM
+socket.on('chat', function (data) {
+    feedback.innerHTML = "";
+    output.innerHTML += "<p><strong>" + data.handle + ":</strong>" + data.message + "</p>"
+});
+
+// receive typing message and output to DOM
+socket.on('typing', function(data){
+    feedback.innerHTML = "<p><em>" + data + " is typing a message...</em></p>";
 });
