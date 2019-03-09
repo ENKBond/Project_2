fd396b67f9c5b0df371f09d703565da74d5a4f6e
Becky Gold <43392694+beckygold@users.noreply.github.com>
2019-03-08 20:54:50 -0700

    Confirm name event, print user list

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,26 +1,43 @@
 // Establish conncetion to server
 // This socket variable is for the frontend (not the server)
-const socket = io();
+// const socket = io();
+const socket = io('http://localhost:8000');
 
 // Query DOM
 const message = document.getElementById('message');
-handle = document.getElementById('handle'),
-    btn = document.getElementById('send'),
+formArea = document.getElementById('form-area'),
+    chatRoom = document.getElementById('chat-room'),
+    nameSelect = document.getElementById('name-select'),
+    username = document.getElementById('username'),
+    confirmBtn = document.getElementById('confirm'),
+    sendBtn = document.getElementById('send'),
+    users = document.getElementById('users'),
     output = document.getElementById('output'),
     feedback = document.getElementById('feedback');
 
-// Emit events to server
+// -- Emit events to server -- 
+// display chat room when user selects a name
+confirmBtn.addEventListener('click', function (e) {
+    console.log(nameSelect.value);
+    e.preventDefault();
+    username.value = nameSelect.value;
+    socket.emit('new user', nameSelect.value)
+    formArea.style.display = 'none';
+    chatRoom.style.display = 'block';
+    nameSelect.value = "";
+});
+
 // On send click, send chat message to server
-btn.addEventListener('click', function () {
+sendBtn.addEventListener('click', function () {
     socket.emit('chat', {
         message: message.value,
-        handle: handle.value
+        username: username.value
     });
 });
 
-// On key press, send handle to server
+// On key press, send username to server
 message.addEventListener('keypress', function () {
-    socket.emit('typing', handle.value);
+    socket.emit('typing', username.value);
 });
 
 
@@ -28,10 +45,19 @@ message.addEventListener('keypress', function () {
 // receive chat message from server and output to DOM
 socket.on('chat', function (data) {
     feedback.innerHTML = "";
-    output.innerHTML += "<p><strong>" + data.handle + ":</strong>" + data.message + "</p>"
+    output.innerHTML += "<p><strong>" + data.username + ":</strong>" + data.message + "</p>"
 });
 
 // receive typing message and output to DOM
-socket.on('typing', function(data){
+socket.on('typing', function (data) {
     feedback.innerHTML = "<p><em>" + data + " is typing a message...</em></p>";
+});
+
+// print users array
+socket.on('get users', function (data) {
+    let html = '';
+    for (i = 0; i < data.length; i++) {
+        html += '<li class="list-group-item">' + data[i] + '</li>'
+    };
+    users.innerHTML = html;
 });
